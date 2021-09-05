23ccf34f49ad3c588b29bdb6c0b84e849132b3fa
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2021-09-05 15:39:39 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 8080;
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(port);
+console.log("Server started at http://localhost:" + port);
