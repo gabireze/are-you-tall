a08ba336054a89ad0979bc134b72301490dd858f
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2021-09-05 17:30:07 -0300

    public folder was removed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 8080;
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname));
 
 app.get("*", function (req, res) {
   res.redirect("/");
